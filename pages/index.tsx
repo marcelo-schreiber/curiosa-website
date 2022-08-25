94334590be592543740017b76052de3b3b55c3cf
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-25 12:28:31 -0300

    add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,19 @@ import "prismjs/themes/prism.css"; //Example style, you can use another
 const Home = () => {
   const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
   return (
-    <Editor
-      value={code}
-      onValueChange={(code) => setCode(code)}
-      highlight={(code) => highlight(code, languages.js, "javascript")}
-      padding={10}
-      style={{
-        fontFamily: '"Fira code", "Fira Mono", monospace',
-        fontSize: 12,
-      }}
-    />
+    <>
+      {/* <Editor
+        value={code}
+        onValueChange={(code) => setCode(code)}
+        highlight={(code) => highlight(code, languages.js, "javascript")}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12,
+        }}
+      /> */}
+      <h1 className="text-lg text-blue-100">working?</h1>
+    </>
   );
 };
 
