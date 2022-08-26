606bff3ea724a46c214b9cc21072adc07445b65f
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-26 19:43:14 -0300

    fix: undefined import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css"; //Example style, you can use another
+// import Editor from "react-simple-code-editor";
+// import { highlight, languages } from "prismjs";
+// import "prismjs/components/prism-clike";
+// import "prismjs/components/prism-javascript";
+// import "prismjs/themes/prism.css"; //Example style, you can use another
 
 const Home = () => {
   const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
