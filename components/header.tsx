40fb9b01535e48a22904737e2ae879af7477affa
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-27 20:49:23 -0300

    change name from femini-alg to curiosa

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ const Header: React.FC = () => {
   return (
     <header className="w-full bg-slate-900 shadow">
       <div className="flex items-center p-5">
-        <Image src="/Logo.svg" alt="femin-alg logo" width={128} height={128} />
-        <h1 className="text-3xl ml-5 text-white">Femini-Alg</h1>
+        <Image src="/Logo.svg" alt="curiosa logo" width={128} height={128} />
+        <h1 className="text-3xl ml-5 text-white">Curiosa</h1>
       </div>
     </header>
   );
