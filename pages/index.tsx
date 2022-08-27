836e82f8125cd48ca39a7fe10802b0275b3c03e5
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-26 23:22:18 -0300

    add: components to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-// import Editor from "react-simple-code-editor";
-// import { highlight, languages } from "prismjs";
-// import "prismjs/components/prism-clike";
-// import "prismjs/components/prism-javascript";
-// import "prismjs/themes/prism.css"; //Example style, you can use another
+import Header from "../components/header";
+import Question from "../components/question";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css"; //Example style, you can use another
 
 const Home = () => {
   const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
   return (
-    <>
-      {/* <Editor
+    <main>
+      <Header />
+      <Question
+        input="1"
+        output="1"
+        title="Title of problem."
+        problem="Given pointers to the head nodes of  linked lists that merge together at some point, find the node where the two lists merge. The merge point is where both lists point to the same node, i.e. they reference the same memory location. It is guaranteed that the two head nodes will be different, and neither will be NULL. If the lists share a common node, return that node's  value."
+      />
+      <Editor
         value={code}
         onValueChange={(code) => setCode(code)}
         highlight={(code) => highlight(code, languages.js, "javascript")}
@@ -18,9 +27,9 @@ const Home = () => {
           fontFamily: '"Fira code", "Fira Mono", monospace',
           fontSize: 12,
         }}
-      /> */}
+      />
       <h1 className="text-lg text-blue-100">working?</h1>
-    </>
+    </main>
   );
 };
 
