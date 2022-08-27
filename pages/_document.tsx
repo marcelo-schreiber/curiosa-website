d96ba55b10b4740f37eab7d5ee364680786743b6
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-27 20:48:22 -0300

    add: meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,7 @@
-import React, { ReactElement } from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { AppProps } from "next/app";
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App() {
   return (
     <Html>
       <Head>
@@ -16,6 +15,19 @@ export default function App({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Curiosa" />
+        <meta
+          name="description"
+          content="Curiosa é uma proposta para reiterar a importante participação feminina na computação, por meio de resolução de exercícios nessa temática."
+        />
+        <meta
+          name="keywords"
+          content="mulheres, computação, python, aprender, código"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Portuguese" />
+        <meta name="author" content="Colocar nome de td mundo dps" />
       </Head>
       <body>
         <Main />
