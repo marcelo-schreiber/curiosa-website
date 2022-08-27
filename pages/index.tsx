1b3a02f8607064ffc9da27ac323239489e4ffe21
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-27 16:38:24 -0300

    fix: output formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,7 @@ print(x1, x2)
         <div className="md:w-1/2">
           <Question
             input="1 2 1"
-            output="-1 -1"
+            output="-1.0 -1.0"
             title="Title of problem."
             imageUrl="/Logo.svg"
             problem="Given pointers to the head nodes of  linked lists that merge together at some point, find the node where the two lists merge. The merge point is where both lists point to the same node, i.e. they reference the same memory location. It is guaranteed that the two head nodes will be different, and neither will be NULL. If the lists share a common node, return that node's  value."
