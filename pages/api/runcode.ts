64cc3d8f6a152211c4c2ebddb15118d7e4085b2f
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-27 12:28:09 -0300

    feat: console.log works and output is returned to user

--- pages/api/runcode.ts
+++ pages/api/runcode.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NodeVM } from "vm2";
+import capcon from "capture-console";
 
 type ResProps = {
   message: string | boolean;
@@ -13,21 +14,22 @@ export default function handler(
   try {
     const body = JSON.parse(req.body);
     const code = body?.code;
-    let isCorrect = false;
 
     if (!code) return res.status(400).json({ message: "Input inválido" });
 
     const vm = new NodeVM({
-      console: "off",
+      console: "inherit",
       timeout: 1000,
       allowAsync: false,
       sandbox: {},
       wrapper: "none",
     });
 
-    if (vm.run(code) === "hello, world") isCorrect = true;
+    const stdout = capcon.captureStdout(() => vm.run(code));
 
-    return res.status(201).json({ message: isCorrect });
+    const finalResult = stdout.replace(/[\r\n]/gm, "");
+
+    return res.status(201).json({ message: finalResult });
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
