d03ad6ef7a261c6ee9d5a77def64a0ccf3bad4c3
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-28 20:37:20 -0300

    remove: unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useState } from "react";
 import Header from "../components/header";
 import Question from "../components/question";
 
