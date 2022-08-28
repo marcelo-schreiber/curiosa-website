3c8b8ed11e996a58b49a7ad9abed3885d30a685b
marcelo-schreiber <maraugrissch@gmail.com>
2022-08-28 15:12:29 -0300

    add: new logo

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,6 @@ const Header: React.FC = () => {
     <header className="w-full bg-slate-900 shadow">
       <div className="flex items-center p-5">
         <Image src="/Logo.svg" alt="curiosa logo" width={128} height={128} />
-        <h1 className="text-3xl ml-5 text-white">Curiosa</h1>
       </div>
     </header>
   );
