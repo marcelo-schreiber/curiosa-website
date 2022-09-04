7ab44e9e725978c67f1b4b31a709bf5ac6feee0d
marcelo-schreiber <maraugrissch@gmail.com>
2022-09-03 21:27:31 -0300

    add: sample second question

--- data/questions.ts
+++ data/questions.ts
@@ -7,4 +7,13 @@ export const questions = [
     output: "-1.0 -1.0",
     imageUrl: "/margaret-hamilton.png",
   },
+
+  {
+    title: "Função quadrática 2",
+    problem:
+      "Faça um programa que solucione uma função quadrática qualquer, recebendo os valores dos índices 'a', 'b' e 'c'.",
+    input: "1 2 1",
+    output: "-1.0 -1.0",
+    imageUrl: "/margaret-hamilton.png",
+  },
 ];
