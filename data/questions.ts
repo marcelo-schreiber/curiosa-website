b64837840f9f5dfaf0bc111e87a1d407b45016e6
Gustan13 <gustan.binder@gmail.com>
2022-09-11 16:33:21 -0300

    add: essays and images

--- data/questions.ts
+++ data/questions.ts
@@ -2,43 +2,43 @@ export const questions = [
   {
     title: "Hello World",
     problem:
-      "Escreva um código que receba uma string e imprima tal string.",
+      "Ada Lovelace nasceu no dia 10 de dezembro de 1815 em Londres, foi abandonada por seu pai enquanto ainda era um bebê, mas sua mãe continuou a cria-la incentivando o interesse pela lógica e pela matemática. Logo que Lovelace começou a trabalhar em um projeto com Charles Babbage, famoso cientista, a incluíram em seu projeto sobre a máquina analítica, que foi usada para executar e imprimir cálculos matemáticos. Sendo a primeira maquina da história que foi capaz de ser programada para efetuar comandos de quaisquer tipos. Entre os anos de 1842-43, Lovelace criou diversas notas sobre a máquina analítica de Charles Babbage, que depois de cem anos foram republicadas, após isso essa máquina foi reconhecida como o primeiro modelo de computador. Posteriormente, as anotações de Ada foram conhecidas como o primeiro algoritmo criado particularmente criado para ser implementado num computador. Após sua morte, ela deixou diversas conquistas mundiais, fazendo com que várias pessoas continuassem o que Ada começou. Ela morreu aos 36 anos de idade, devido a um câncer no útero.\nEscreva um código que receba uma string e imprima tal string.",
     input: "Macaco",
     output: "Macaco",
-    imageUrl: "/margaret-hamilton.png",
+    imageUrl: "/ada-lovelace.jpg",
   },
   
   {
     title: "Multiplicações",
     problem:
-      "Escreva um código que receba três números a, b e c e imprima o produto dos três.",
+      "Katherine Johnson, nasceu em 1918 na Virgínia Ocidental e com apenas 15 anos já havia ingressado na West Virgínia State College, onde cursou todos os cursos que estudavam matemática. Com o apoio do professor W. W. Schieffelin Claytor, decidiu investir na carreira de pesquisadora na área de matemática. Em 1950, com 18 anos, se inscreveu e foi escolhida para uma vaga na NASA, por conta da sua esperiência com equações complexas de geometria, conseguiu uma vaga para trabalhar no projeto Mercury, de 1961. Após isso, participou da equipe que formulou a rota da primeira missão tripulada à Lua, em 1969. E la ficou encarregada de realizar a maioria dos cálculos, além de verificar as contas que eram realizadas nos computadores da NASA. Após sua participação no Projeto Apollo, Katherine Johnson trabalhou em diversas operações da NASA, sendo um dos mais importantes o programa dos ônibus espaciais e nos planos para a missão a Marte. Ela se aposentou em 1986, depois de 33 anos de trabalho. O reconhecimento pelos seus serviços veio em 2015, quando recebeu do então presidente, Barack Obama, o prêmio Presidential Medal of Freedom, conhecido como a maior horaria que se pode receber nos Estados Unidos. E então, em 2016, a NASA criou uma central de pesquisa com seu nome, como forma de homenagem, além disso, em 2016 estreou o filme Hidden Figures (Estrelas além do tempo) baseado na história de Katherine e outras duas Matemáticas Dorothy Vaughn e Mary Jackson. Ela faleceu com 102 anos, no ano de 2020.\nEscreva um código que receba três números a, b e c e imprima o produto dos três.",
     input: "2 5 7",
     output: "70",
-    imageUrl: "/margaret-hamilton.png",
+    imageUrl: "/katherine-johnson",
   },
   
   {
     title: "Paridade",
     problem:
-      "Escreva um código que receba um número X e imprima 'PAR' se o número for par e 'IMPAR' se o número for ímpar.",
+      "Nasceu no dia 2 de março de 1922 na Filadélfia, ela estudou na South Philadelphia High School for Girls e se formou em 1938. frequentou a Temple University, mas mudou para Chestnut Hill College depois de receber uma bolsa de estudos. Ela se formou em matemática com especialização em física e se formou em 1942.Foi uma das programadoras originais do ENIAC (o primeiro computador digital eletrônico). Ela é considerada uma das primeiras programadoras de computador da história. foi introduzido no Women in Technology International Hall of Fame em 1997 junto com diversos programadores do ENIAC. Graças a seus grandes trabalhos ela abriu diversos caminhos para os futuros computadores, dando início a chegada da computação eletrônica e da programação de computadores na era pós-Segunda Guerra Mundial. Ela faleceu no dia 8 de julho de 2012.\nEscreva um código que receba um número X e imprima 'PAR' se o número for par e 'IMPAR' se o número for ímpar.",
     input: "7",
     output: "IMPAR",
-    imageUrl: "/margaret-hamilton.png",
+    imageUrl: "/frances-bilas-spence.jpg",
   },
   
   {
     title: "Complicações",
     problem:
-      "Escreva um código que receba um número X e imprima 'HUZZAH' se o quadrado de X for divisível por 3, senão imprima 'DISHONOR'.",
+      "Ela nasceu no dia 7 de março de 1917 na Filadélfia, juntamente com frances bilas, ela foi uma das outras sei programadoras originais do primeiro computador digital de uso geral ENIAC. Se formou em jornalismo na universidade da Pensilvânia em 1940. Ela foi escolhida pelo ENIAC para calcular as trajetórias balísticas, durante a segunda guerra mundial.Após a guerra ela trabalhou na Remington Rand, um fabricante de máquinas comerciais e depois no National Bureau of Standards, um laboratório de padrões de medição. Aqui ela trabalhou nas duas primeiras revisões do Epadrão de linguagem FORTRAN,Em 1959, ela era a Chefe do Ramo de Pesquisa de Programação, Laboratório de Matemática Aplicada na David Taylor Model Basin, uma das maiores instalações de teste de bacias de modelos de navios do mundo. Ela sofria de uma doença cardíaca e acabou morrendo em 8 de Dezembro de 2001.\nEscreva um código que receba um número X e imprima 'HUZZAH' se o quadrado de X for divisível por 3, senão imprima 'DISHONOR'.",
     input: "6",
     output: "HUZZAH",
-    imageUrl: "/margaret-hamilton.png",
+    imageUrl: "/betty-snyder.jpg",
   },
 
   {
     title: "Função quadrática",
     problem:
-      "Faça um programa que solucione uma função quadrática qualquer, recebendo os valores dos índices 'a', 'b' e 'c'. Porém, imprima a soma das soluções",
+      "Ela nasceu no dia 17 de agosto de 1936 em Paoli (Indiana, EUA), trabalhou para a NAS no projeto Apollo 11, onde desenvolveu um software que permitiu que pela primeira vez o homem chegasse a lua, sendo ela a primeira engenheira de software. Em 2016 recebeu a Medalha Presidencial da Liberdade das mãos de Barack Obama como homenagem ao trabalho realizado na NASA. Formou-se no ensino médio em 1954 na Hancock High School e se formou na faculdade em 1958, se dedicando aos estudos da matemática, fez uma pós-graduação em meteorologia no Instituto de Tecnologia de Massachusetts (MIT). Trabalhou como professora de matemática até que em 1960 começou a trabalhar no MIT desenvolvendo programas meteorológicos.\nFaça um programa que solucione uma função quadrática qualquer, recebendo os valores dos índices 'a', 'b' e 'c'. Porém, imprima as soluções",
     input: "1 2 1",
     output: "-2.0",
     imageUrl: "/margaret-hamilton.png",
