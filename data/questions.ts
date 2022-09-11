afd373a9d94918f247aa0103650cd0170f0828cd
Gustan13 <gustan.binder@gmail.com>
2022-09-11 15:39:49 -0300

    Add: More questions

--- data/questions.ts
+++ data/questions.ts
@@ -1,15 +1,42 @@
 export const questions = [
   {
-    title: "Função quadrática",
+    title: "Hello World",
     problem:
-      "Faça um programa que solucione uma função quadrática qualquer, recebendo os valores dos índices 'a', 'b' e 'c'.",
-    input: "1 2 1",
-    output: "-1.0 -1.0",
+      "Escreva um código que receba uma string e imprima tal string.",
+    input: "Macaco",
+    output: "Macaco",
+    imageUrl: "/margaret-hamilton.png",
+  },
+  
+  {
+    title: "Multiplicações",
+    problem:
+      "Escreva um código que receba três números a, b e c e imprima o produto dos três.",
+    input: "2 5 7",
+    output: "70",
+    imageUrl: "/margaret-hamilton.png",
+  },
+  
+  {
+    title: "Paridade",
+    problem:
+      "Escreva um código que receba um número X e imprima 'PAR' se o número for par e 'IMPAR' se o número for ímpar.",
+    input: "2 5 7",
+    output: "70",
+    imageUrl: "/margaret-hamilton.png",
+  },
+  
+  {
+    title: "Complicações",
+    problem:
+      "Escreva um código que receba um número X e imprima 'HUZZAH' se o quadrado de X for divisível por 3, senão imprima 'DISHONOR'.",
+    input: "6",
+    output: "HUZZAH",
     imageUrl: "/margaret-hamilton.png",
   },
 
   {
-    title: "Função quadrática 2",
+    title: "Função quadrática",
     problem:
       "Faça um programa que solucione uma função quadrática qualquer, recebendo os valores dos índices 'a', 'b' e 'c'. Porém, imprima a soma das soluções",
     input: "1 2 1",
