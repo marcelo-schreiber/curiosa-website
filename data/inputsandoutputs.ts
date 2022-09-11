681fefd85491024b5f029ca00ab2af3ea2b73771
Gustan13 <gustan.binder@gmail.com>
2022-09-11 15:55:09 -0300

    add: inputs and outputs to new questions

--- data/inputsandoutputs.ts
+++ data/inputsandoutputs.ts
@@ -6,9 +6,30 @@ export type Question = {
 // BINDER
 export const inputsAndOutputs: Question[][] = [
   [
+    {input: "Mamaco", output: "Mamaco\n"},
+    {input: "Macankey", output: "Macankey\n"}
+  ],
+  [
+    {input: "5\n8\n9", output: "360\n"},
+    {input: "7\n1\n1", output: "7\n"},
+    {input: "40\n2\n3", output: "240\n"}
+  ],
+  [
+    {input: "1", output: "IMPAR\n"},
+    {input: "2", output: "PAR\n"},
+    {input: "40", output: "PAR\n"},
+    {input: "24", output: "PAR\n"},
+    {input: "23", output: "IMPAR\n"}
+  ],
+  [
+    {input: "2", output: "DISHONOR\n"},
+    {input: "3", output: "HUZZAH\n"},
+    {input: "9", output: "HUZZAH\n"},
+    {input: "5", output: "DISHONOR\n"}
+  ],
+  [
     { input: "1\n2\n1", output: "-1.0 -1.0\n" },
     { input: "3\n-1\n-2", output: "1.0 -0.6666666666666666\n" },
   ],
-  [{ input: "1\n2\n1", output: "-2.0\n" }],
 ];
 // BINDER
